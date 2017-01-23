bd6206c4432e9f227bf2849f5c2e49598a3be8ea
rexicq <rexicq@gmail.com>
2017-01-23 20:09:28 +0200

    'Init'

---
+++ src/components/post_index.js
@@ -0,0 +1,27 @@
+/**
+ * Created by shitman on 23.01.2017.
+ */
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/index';
+import { Link } from 'react-router';
+
+class PostsIndex extends Component{
+	componentWillMount() {
+		this.props.fetchPosts();
+	}
+	render() {
+		return (
+			<div>
+				<div className="text-xs-right">
+					<Link to="/posts/new" className="btn btn-primary">
+						Add a post
+					</Link>
+				</div>
+				List of blog
+			</div>
+		);
+	}
+}
+
+export default connect(null, { fetchPosts })(PostsIndex)
